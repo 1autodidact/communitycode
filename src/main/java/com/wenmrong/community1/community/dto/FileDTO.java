3fce48ca39cd7d3d25501ecff97e6b3228d3e7e9
1autodidact <1985013202@qq.com>
2020-03-17 09:37:50 +0800

    Markdown implement uploading images

---
+++ src/main/java/com/wenmrong/community1/community/dto/FileDTO.java
@@ -0,0 +1,10 @@
+package com.wenmrong.community1.community.dto;
+
+import lombok.Data;
+
+@Data
+public class FileDTO {
+    private int success;
+    private String message;
+    private String url;
+}
