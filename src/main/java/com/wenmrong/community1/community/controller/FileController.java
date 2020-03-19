3077ef1ac5d77cb62c628c511e5a4d186bb6152c
1autodidact <1985013202@qq.com>
2020-03-19 08:14:18 +0800

    fix image for uploading failure

---
+++ src/main/java/com/wenmrong/community1/community/controller/FileController.java
@@ -0,0 +1,19 @@
+package com.wenmrong.community1.community.controller;
+
+import com.wenmrong.community1.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/iceman.png");
+        return fileDTO;
+    }
+
+}
