cfcd48310722f45f5ce7c512218dbc9097af7b54
1autodidact <1985013202@qq.com>
2020-03-19 23:17:31 +0800

    fix log with many peoples

--- src/main/java/com/wenmrong/community1/community/service/CommentService.java
+++ src/main/java/com/wenmrong/community1/community/service/CommentService.java
@@ -77,6 +77,8 @@ public class CommentService {
             if (question == null) {
                 throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
             }
+            //INTEGER类型模型默认初始化的时候为null,尽管数据库default=0
+            comment.setCommentCount(0);
             commentMapper.insert(comment);
             question.setCommentCount(1);
             questionExtMapper.incCommentCount(question);
@@ -85,6 +87,9 @@ public class CommentService {
     }
 
     private void createNotification(Comment comment, Long receiver, String notifierName, String outerTitle, NotificationTypeEnum notificationType, Long outerId) {
+        if (receiver == comment.getCommentator()) {
+            return;
+        }
         Notification notification = new Notification();
         notification.setGmtCreate(System.currentTimeMillis());
         notification.setType(notificationType.getType());
