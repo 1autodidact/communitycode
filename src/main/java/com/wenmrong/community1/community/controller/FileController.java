3ec2d40525a0dd5c50864b382ef66f53b585552a
1autodidact <1985013202@qq.com>
2020-03-19 08:53:27 +0800

    fix uploading fuction

--- src/main/java/com/wenmrong/community1/community/controller/FileController.java
+++ src/main/java/com/wenmrong/community1/community/controller/FileController.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FileController {
-<<<<<<< HEAD
     @RequestMapping("/file/upload")
-=======
-    @RequestMapping("file/upload")
->>>>>>> 3fce48ca39cd7d3d25501ecff97e6b3228d3e7e9
     @ResponseBody
     public FileDTO upload(){
         FileDTO fileDTO = new FileDTO();
