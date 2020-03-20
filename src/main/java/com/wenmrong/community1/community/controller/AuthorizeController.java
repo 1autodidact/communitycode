976729148f564c08fce271d09739f0e1bb13bb4b
1autodidact <1985013202@qq.com>
2020-03-20 22:09:31 +0800

    manage log

--- src/main/java/com/wenmrong/community1/community/controller/AuthorizeController.java
+++ src/main/java/com/wenmrong/community1/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.wenmrong.community1.community.dto.GithubUser;
 import com.wenmrong.community1.community.model.User;
 import com.wenmrong.community1.community.provider.GithubProvider;
 import com.wenmrong.community1.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
@@ -55,6 +57,7 @@ public class AuthorizeController {
             response.addCookie(new Cookie("token", token));
             return "redirect:/";
         } else {
+            log.error("callback return github error {}",githubUser);
             return "redirect:/";
         }
     }
