6f5e3366d88bd15187f65f5daea927baa6b90012
1autodidact <1985013202@qq.com>
2020-04-20 10:59:00 +0800

    Intercept thumb without login

--- src/main/java/com/wenmrong/community1/community/interceptor/WebConfig.java
+++ src/main/java/com/wenmrong/community1/community/interceptor/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/profile/replies","/profile/questions","/publish","/wallpaper","/download/**","/profile/questions/delete/**");
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/profile/replies","/profile/questions","/publish","/wallpaper","/download/**","/profile/questions/delete/**","/thumb/**");
 
 
     }
