f6e48af3f3947501c6ed9951fd728ba56dedc5b8
1autodidact <1985013202@qq.com>
2020-04-20 16:19:36 +0800

    modfiy pagination logic and optimize page

--- src/main/java/com/wenmrong/community1/community/service/QuestionService.java
+++ src/main/java/com/wenmrong/community1/community/service/QuestionService.java
@@ -59,7 +59,7 @@ public class QuestionService {
             page = totalPage;
         }
         paginationDTO.setPagination(totalPage, page);
-        Integer offset = size * (page - 1);
+        Integer offset = page < 1 ? 0 : size * (page - 1);
         QuestionExample questionExample = new QuestionExample();
         questionExample.setOrderByClause("gmt_create desc");
         questionQueryDTO.setPage(offset);
